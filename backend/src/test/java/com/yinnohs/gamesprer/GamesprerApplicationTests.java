8e9fafff9bdd6347145bd45445dacaf7e2ce3634
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-01-21 22:51:33 +0000

    Feature :sparkles: (fronted-backend): created dockerfile for backend

--- backend/src/test/java/com/yinnohs/gamesprer/GamesprerApplicationTests.java
+++ backend/src/test/java/com/yinnohs/gamesprer/GamesprerApplicationTests.java
@@ -3,11 +3,5 @@ package com.yinnohs.gamesprer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GamesprerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
