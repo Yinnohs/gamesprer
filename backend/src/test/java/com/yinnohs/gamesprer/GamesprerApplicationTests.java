43a2869ded2a16debc92c8237c1949217cf37c1b
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-01-25 16:49:53 +0000

    Test :test_tube: (back): added arch test and test containers

--- backend/src/test/java/com/yinnohs/gamesprer/GamesprerApplicationTests.java
+++ backend/src/test/java/com/yinnohs/gamesprer/GamesprerApplicationTests.java
@@ -1,4 +1,25 @@
 package com.yinnohs.gamesprer;
 
-class GamesprerApplicationTests {
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MongoDBContainer;
+
+@SpringBootTest
+public class GamesprerApplicationTests {
+    final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10");
+
+    @BeforeAll
+    public void setUp() {
+        mongoDBContainer.start();
+        String connectioString = mongoDBContainer.getConnectionString();
+        System.setProperty("spring.data.mongodb.uri", connectioString);
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+
+
 }
---
+++ backend/src/test/java/com/yinnohs/gamesprer/architechture/HexagonalArchTest.java
@@ -0,0 +1,41 @@
+package com.yinnohs.gamesprer.architechture;
+
+import org.junit.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.lang.ArchRule;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+@AnalyzeClasses(packages = "com.yinnohs.gamesprer")
+public class HexagonalArchTest {
+    private final String BASE_PACKAGE = "com.yinnohs.gamesprer";
+    private final String JAVA_PACKAGES = "java..";
+    private final String DOMAIN_PACKAGES = "..domain..";
+    private final String APPLICATION_PACKAGES =  "..application..";
+    private final String LOMBOK = "lombok..";
+    private  JavaClasses importedClasses = new ClassFileImporter().importPackages(BASE_PACKAGE);
+
+     @Test
+    public void ensure_domain_classes_only_depends_on_java_std_library_and_lombok(){
+        ArchRule domainRule = noClasses().that()
+                .resideInAnyPackage(DOMAIN_PACKAGES)
+                .should()
+                .dependOnClassesThat()
+                .resideOutsideOfPackages(DOMAIN_PACKAGES, JAVA_PACKAGES, LOMBOK);
+
+        domainRule.allowEmptyShould(true).check(importedClasses);
+    }
+
+    @Test
+    public void ensure_application_classes_only_depends_on_java_std_library_lombok_domain_and_user_domain(){
+        ArchRule domainRule = noClasses().that()
+                .resideInAnyPackage(APPLICATION_PACKAGES)
+                .should()
+                .dependOnClassesThat()
+                .resideOutsideOfPackages(DOMAIN_PACKAGES, JAVA_PACKAGES, LOMBOK, APPLICATION_PACKAGES);
+
+        domainRule.allowEmptyShould(true).check(importedClasses);
+    }
+}
