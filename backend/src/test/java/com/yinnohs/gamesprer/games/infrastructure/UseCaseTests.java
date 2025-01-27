8975d4c93f5e8f020b5035b67c5e4a4ca3581e73
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-01-27 19:39:21 +0000

    Build (back): added required metadata

--- backend/src/test/java/com/yinnohs/gamesprer/games/infrastructure/UseCaseTests.java
+++ backend/src/test/java/com/yinnohs/gamesprer/games/infrastructure/UseCaseTests.java
@@ -1,11 +1,8 @@
 package com.yinnohs.gamesprer.games.infrastructure;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.util.Date;
+
 import java.util.List;
 import java.util.UUID;
 
