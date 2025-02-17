2d43fde03029237475676ebb47057ebc4316e86d
yinnohs (jose ignacio soto leon) <joistleon@gmail.com>
2025-02-17 19:39:00 +0000

    Feature :sparkles: (back): created notification domain representation

---
+++ backend/src/main/java/com/yinnohs/gamesprer/notification/domain/Notification.java
@@ -0,0 +1,20 @@
+package com.yinnohs.gamesprer.notification.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Notification {
+    private String id;
+    private String title;
+    private String message;
+    private String type;
+    private String UserId;
+}
